4f2fa1889893acb8b321457d76aa6ed202ec9c72
Karen Ngo <karen12ngo@gmail.com>
2017-11-02 01:07:34 -0400

    Added ability to add/rm students from files

--- Deliverable 03/CSCC01 Statistics app/src/Main/Data.java
+++ Deliverable 03/CSCC01 Statistics app/src/Main/Data.java
@@ -1,10 +1,13 @@
 package Main;
 
+import java.io.*;
 import java.util.*;
 
 public class Data {
     static Map Users = new HashMap();
     static ArrayList <Student> studentList;
+    public static final String USERSTXT = "users.txt";
+    public static final String TEMPTXT = "tempUsers.txt";
 
     public Data (){
     }
@@ -14,8 +17,40 @@ public class Data {
         System.out.println(id+" added");
     }
 
+    public void writeNewUser(String id, String pw) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(USERSTXT, true))) {
+            bw.write(id + ", " + pw);
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void RemoveUser (String id){
         Users.remove(id);
+    }
+
+    public void removeFromFile(String id) {
+        try {
+            File inputFile = new File(USERSTXT);
+            File tempFile = new File(TEMPTXT);
+
+            BufferedReader br = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+
+            String currLine;
+            while ((currLine = br.readLine()) != null) {
+                if (currLine.split(",")[0].equals(id)) continue;
+                bw.write(currLine + "\n");
+            }
+
+            bw.close();
+            br.close();
+            tempFile.renameTo(inputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static boolean CheckUser (String id, String pw){
