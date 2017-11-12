00571ba1691aeff3d9dc7a5c4222747446e1c927
Karen Ngo <karen12ngo@gmail.com>
2017-11-12 17:38:39 -0500

    Everything related to saving assignments seems to be working

--- Deliverable 04/CSCC01 Statistics app/src/Main/Student.java
+++ Deliverable 04/CSCC01 Statistics app/src/Main/Student.java
@@ -15,6 +15,7 @@ public class Student extends User{
     public SimpleStringProperty fName;
     SimpleStringProperty lName;
     SimpleIntegerProperty studentId;
+    // Mapping explained below
     private HashMap<String, ArrayList<ArrayList<String>>> completedAssignments = new HashMap<>();
     private HashMap<String, ArrayList<Double>> assignmentMarks = new HashMap<>();
 
@@ -66,13 +67,16 @@ public class Student extends User{
 
     public Double getBestMarkFor(String assgnName) {
         Double bestMark = 0.0;
+        System.out.println(" hellooo");
         if(assignmentMarks.containsKey(assgnName)) {
             for(Double mark : assignmentMarks.get(assgnName)) {
+                System.out.println(" -- mark " + mark);
                 if(mark > bestMark) {
                     bestMark = mark;
                 }
             }
         }
+        System.out.println(" -- best " + bestMark);
         return bestMark;
     }
 
@@ -88,6 +92,13 @@ public class Student extends User{
         }
     }
 
+    /*
+    MAP KEY: assignment name
+    MAPPED TO: an arraylist containing 2 string arraylists
+        First list: the questions listed in order (just the actual question string ie. "What is SS16?")
+        Second list: the indexes of the selected answers (they're all strings though)
+        Looks like something like:  "A1" --> [  ["Q1", "Q2", "Q3"], ["1", "0", "3"]  ]
+     */
     public HashMap<String, ArrayList<ArrayList<String>>> getCompletedAssignments() {
         return completedAssignments;
     }
